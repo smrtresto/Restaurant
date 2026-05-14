b2667200b3482ad0a75e1d13367e7dd889fdeb52
smrtresto <smrtresto@gmail.com>
2026-05-14 20:14:43 +0530

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 // supabase.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'https://ciixjaljoxrziyebmfuj.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_rqQb3sXdRIBQKQ9j-BV76Q_2UeukIL3';
+const SUPABASE_URL = 'https://tvuvjpwfyhtyfozzbxlw.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_17Wo7UFYkSLdOi6Zcectvg_46SUUEhC';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
